91038f69cf64e92401758472c21b70b030c8cc5d
Corporate Gadfly <haroon.rafique@gmail.com>
2026-09-15 18:42:31 -0400

    replace deprecated calls with Facter::Core::Execution
    Signed-off-by: Corporate Gadfly <haroon.rafique@gmail.com>

--- lib/facter/nm_version.rb
+++ lib/facter/nm_version.rb
@@ -15,7 +15,7 @@ Facter.add(:nm_version) do
   confine { Facter::Core::Execution.which('NetworkManager') }
 
   setcode do
-    Facter::Util::Resolution.exec('NetworkManager --version').strip
+    Facter::Core::Execution.execute('NetworkManager --version').strip
   rescue StandardError
     nil
   end
